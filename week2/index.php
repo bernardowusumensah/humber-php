325c35797bb540c7c3bce4c8ec91338d6fd27cad
Bernard Owusu-Mensah <tgatelbernard@gmail.com>
2025-05-14 16:38:29 -0400

    week 2

---
+++ week2/index.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<html  lang="">
+<head>
+    <title>Week 2</title>
+</head>
+
+<body>
+
+<!--learning echo in php-->
+
+<?php
+
+
+echo "<h1>Hello World </h1>";
+$firstname = "Bernard";
+$lastname = "Owusu-Mensah";
+echo "$lastname  $firstname";
+?>
+</body>
+
+</html>
+
+
+
+
